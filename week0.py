08e68b732d5775f5797d6f18923974e71e99ed76
Wu Kwok Wai <45204032+frankie6589@users.noreply.github.com>
2024-03-14 22:09:24 +0800

    Update week0.py

--- week0.py
+++ week0.py
@@ -13,27 +13,6 @@
 # HINT: using % operator to find remainder may be helpful
 def dose(needs):
     #YOUR SOLUTION STARTS HERE
-    total_needs = sum(needs)
-    
-    if total_needs > 500:
-        return "No medicine given"
-    
-    medicine_list = []
-    vitamins = 0
-    attribute = 0
-    for attribute in needs:
-        if attribute >= 250:
-            return "No medicine given"
-        vitamins = attribute // 10
-        
-        if attribute % 10 !=0:
-            vitamins += 1
-        injections = vitamins*10 -attribute
-        print(injections, vitamins)
-        medicine_list.append((vitamins, injections))
 
-    return medicine_list
-
-# print(dose([250, 0, 250, 0, 0, 0]))
     #YOUR SOLUTION ENDS HERE
 
